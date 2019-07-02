9ea8245c5bddd449c4dc50d609878d340979b00c
tanyagoyal15 <tanyagoyal15@gmail.com>
2019-07-02 06:14:09 +0530

    added Channels

--- src/App.js
+++ src/App.js
@@ -1,44 +1,59 @@
 import React, { Component } from 'react';
 import Channels from './components/Channels';
 import News from './components/News';
+import Filter from './components/Filter';
 
 export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      channel : '',
-      articles : []
+      articles : [],
+      channels : [],
+      sourceClicked : '',
+      category : ''
     }
+    this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleChangeCountry = this.handleChangeCountry.bind(this);
+    // this.handleChangeCategory = this.handleChange.bind(this);
     this.fetchNews = this.fetchNews.bind(this);
   }
 
-  async fetchNews(channel) {
+  componentDidMount() {
+    fetch('https://newsapi.org/v2/sources?apiKey=728c6352a8444ac4a0cd7dd69715a3f2')
+    .then(response => response.json())
+    .then(response => this.setState({
+      channels : response.sources
+    }))
+  }
 
-     fetch("https://newsapi.org/v2/top-headlines?country=de&category=" + channel + "&apiKey=728c6352a8444ac4a0cd7dd69715a3f2")
+  handleChange(e) {
+    this.setState({sourceClicked: e.target.value});
+    console.log("Hello");
+  }
+
+  handleClick(e) {
+    console.log(this.state.sourceClicked);
+    this.fetchNews(this.state.sourceClicked);
+  }
+
+  fetchNews(sourceClicked) {
+    fetch("https://newsapi.org/v2/top-headlines?sources=" + sourceClicked +"&apiKey=728c6352a8444ac4a0cd7dd69715a3f2")
     .then(response => response.json())
     .then(response => this.setState({
       articles : response.articles
     }))
   }
 
-  handleChange(e) {
-  this.setState({channel: e.target.value});
-  }
-
-  handleSubmit(e) {
-    console.log(this.state.channel);
-    this.fetchNews(this.state.channel);
-  }
-
   render() {
   return (
     <div className="App">
-      <h1>New App</h1>
-      <Channels value={this.state.channel}
-                handleChange={this.handleChange}
-                handleSubmit={this.handleSubmit}/>
+      <h1>News App</h1>
+      <Filter     handleChangeCountry={this.handleChangeCountry} 
+                  handleChangeCategory={this.handleChangeCategory} />
+      <Channels channels={this.state.channels} sourceClicked={this.state.sourceClicked}
+                handleClick={this.handleClick}
+                handleChange={this.handleChange}/>
       <News articles={this.state.articles}/>
     </div>
   );
--- src/components/Channels.js
+++ src/components/Channels.js
@@ -5,20 +5,13 @@ export default class Channels extends Component {
 		return (
 			<div className="container">
 				<div className="row">
-					<label>
-			          Select City:
-			          <select value={this.props.channel} onChange={(e) => this.props.handleChange(e)}>
-			            <option value="" disabled>Select Channel</option>
-			            <option value="business">Business</option>
-						<option value="entertainment">Entertainment</option>
-						<option value="general">General</option>
-						<option value="health">Health</option>
-						<option value="science">Science</option>
-						<option value="sports">Sports</option>
-						<option value="technology">Technology</option>
-			          </select>
-			            <input type="submit" value="Submit" onClick={(e) => this.props.handleSubmit(e)}/>
-			        </label>
+			  <select value={this.props.value} onChange={(e) => this.props.handleChange(e)} >
+		  			<option value="">Select Channel</option>
+			  	{this.props.channels.map((channel) => {
+			  		return <option value={channel.id}>{channel.name}</option>
+			  	})}
+			  </select>
+			    <input type="submit" value="Submit" onClick={(e) => this.props.handleClick(e)}/>
 				</div>
 			</div>
 		)
